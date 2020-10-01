f05cb05a4e164a48d608886baaaf5cecdd7af5c4
romainm <romoreaux@gmail.com>
2020-10-01 14:43:48 +0200

    basketproduct + commercialOffer

--- src/components/SubTotal.js
+++ src/components/SubTotal.js
@@ -2,18 +2,22 @@ import React from 'react'
 import { createUseStyles } from 'react-jss'
 import CurrencyFormat from 'react-currency-format'
 import Button from './Button'
-import { useBasketContext } from './Basket'
-import { getBasketTotal } from './Basket'
+import {
+  getBasketTotal,
+  getCommercialOffers,
+  useBasketContext,
+  getTotalwithBestOffer,
+} from './Basket'
+import useFetchApi from './useFetch'
 
 const useStyles = createUseStyles((theme) => ({
   subtotal: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
-    width: 300,
     padding: theme.spaces.md,
 
-    backgroundColor: '#fff',
+    backgroundColor: theme.colors.grey,
     border: `1px solid ${theme.colors.grey}`,
     borderRadius: 4,
     '& p': { color: theme.colors.raven },
@@ -22,34 +26,73 @@ const useStyles = createUseStyles((theme) => ({
     marginLeft: theme.spaces.md,
     fontWeight: 600,
   },
-  gift: { display: 'flex', alignItems: 'center' },
+  gift: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: theme.spaces.md,
+  },
 }))
 
 function SubTotal() {
   const classes = useStyles()
   const [{ basket }] = useBasketContext()
+
+  let isError,
+    isLoading = false
+  let offers = [
+    {
+      type: 'percentage',
+      value: 5,
+    },
+    {
+      type: 'minus',
+      value: 15,
+    },
+    {
+      type: 'slice',
+      sliceValue: 100,
+      value: 12,
+    },
+  ]
+
+  // const [{ data, isLoading, isError }] = useFetchApi(
+  //   `http://henri-potier.xebia.fr/books/${getCommercialOffers(
+  //     basket
+  //   )}/commercialOffers`,
+  //   []
+  // )
+
   return (
     <div className={classes.subtotal}>
-      <CurrencyFormat
-        renderText={(value) => (
-          <>
-            <p>
-              Subtotal ({basket?.length} items):
-              <strong className={classes.total}>{value}</strong>
-            </p>
-            <small className={classes.gift}>
-              <input type="checkbox" />
-              this order contain a gift
-            </small>
-            <Button model="secondary">Procced to checkout</Button>
-          </>
-        )}
-        decialScale={2}
-        value={getBasketTotal(basket)}
-        displayType={'text'}
-        thousandSeparator={true}
-        prefix={'€'}
-      />
+      {isError && (
+        <p className={classes.statusIndicator}>
+          Une erreur est survenue, veuillez rafraîchir votre page...
+        </p>
+      )}
+      {isLoading ? (
+        <p className={classes.statusIndicator}>Ressources en chargement</p>
+      ) : (
+        <CurrencyFormat
+          renderText={(value) => (
+            <>
+              <p>
+                Subtotal ({basket?.length} items):
+                <strong className={classes.total}>{value}</strong>
+              </p>
+              <small className={classes.gift}>
+                <input type="checkbox" />
+                this order contain a gift
+              </small>
+              <Button model="primary">Procced to checkout</Button>
+            </>
+          )}
+          decialScale={2}
+          value={getTotalwithBestOffer(offers, getBasketTotal(basket))}
+          displayType={'text'}
+          thousandSeparator={true}
+          prefix={'€'}
+        />
+      )}
     </div>
   )
 }
